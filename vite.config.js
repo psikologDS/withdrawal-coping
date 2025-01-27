9163fa2dc5c0271979cb9a55975670db00643c79
psikologDS <tugbaunsel@gmail.com>
2025-01-27 22:15:10 +0300

    güncelleme

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-tailwind-project/"
+  base: "/withdrawal-coping/", // Buraya kendi repo adını ekledik
 });
